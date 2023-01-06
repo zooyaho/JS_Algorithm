0606d39ff40c85529135a188db13b1cc19579aa4
zooyaho <84559229+zooyaho@users.noreply.github.com>
2023-01-06 16:16:40 +0900

    [level 2] Title: 짝지어 제거하기, Time: 87.70 ms, Memory: 58.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,14 @@
+function solution(s){
+    var stack = [];
+    
+    for(let str of s){
+        if(stack.length > 0 && stack.at(-1) === str){
+            // stack = stack.slice(0,-1);
+            stack.pop();
+        } else {
+            stack.push(str);
+        }
+    }
+
+    return stack.length === 0 ? 1 : 0;
+}
