82636cf4eca7e8729788b2637edec76cbff292a0
zooyaho <84559229+zooyaho@users.noreply.github.com>
2023-01-16 14:45:12 +0900

    [level 2] Title: [3차] n진수 게임, Time: 2.95 ms, Memory: 34.7 MB -BaekjoonHub

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
@@ -4,14 +4,11 @@ function solution(n, t, m, p) {
     var i = 0;
     while(list.length < t * m){
         var word = i.toString(n);
-        list.push(...word.split("").map((v)=>{
-            if(/[a-z]/.test(v)) return v.toUpperCase();
-            else return v;
-        }));
+        list.push(...word);
         i++;
     }
     for(let i = 0; (i * m) + p <= t*m; i++){
         answer.push(list[(i * m) + p - 1]);
     }
-    return answer.join("");
+    return answer.join("").toUpperCase();
 }
