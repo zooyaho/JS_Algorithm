49da1340bef0c3727542211876186ea2fa5f3d68
zooyaho <84559229+zooyaho@users.noreply.github.com>
2023-01-19 17:26:32 +0900

    [level 2] Title: 오픈채팅방, Time: 114.60 ms, Memory: 95.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,13 +1,13 @@
 function solution(record) {
-    record = record.map((v)=>v.split(" "));
     var userName = {}
     var print = [];
     
     record.forEach((data) => {
-        if(data[0] === 'Enter' || data[0] === 'Change')
-            userName[data[1]] = data[2];
-        if(data[0] === 'Enter' || data[0] === 'Leave')
-            print.push([data[0], data[1]]);
+        var [state, uid, name] = data.split(" ");
+        if(state === 'Enter' || state === 'Change')
+            userName[uid] = name;
+        if(state === 'Enter' || state === 'Leave')
+            print.push([state, uid]);
     });
     print = print.map((v) => {
         if(v[0] === 'Enter') return `${userName[v[1]]}님이 들어왔습니다.`
